f5ba69077b77dadc915bb6f85aa20ef8975d03e6
Jordy Diaz <diaz.jordyb@gmail.com>
2024-08-16 15:24:11 -0500

    final

--- src/Ejecutables/EjecutableDeclaraciones.java
+++ src/Ejecutables/EjecutableDeclaraciones.java
@@ -3,6 +3,10 @@ package Ejecutables;
 public class EjecutableDeclaraciones {
 
     public static void main(String[] args) {
+        declararNumerosEnteros();
+        declararNumerosDecimales();
+        declararTextos();
+        declararArreglos();
 
     }
 
@@ -16,7 +20,7 @@ public class EjecutableDeclaraciones {
         System.out.println(entero1*entero2*entero3*entero4);
     }
 
-    public static void declararNumerosEDecimales(){
+    public static void declararNumerosDecimales(){
 
         double decimal1=2.3;
         double decimal2=43.3;
@@ -27,9 +31,48 @@ public class EjecutableDeclaraciones {
 
     }
 
-    public static void declararTexto(){
+    public static void declararTextos(){
 
         char letra1='a';
         char letra2='b';
+
+        System.out.println(String.format("%s y %s", letra1, letra2));
+
+        String texto1="hola";
+        String texto2="mundo";
+
+        System.out.println(texto1 + texto2);
+
     }
+
+    public static void declararArreglos(){
+
+        int numeros [] = {1,2,3,4};
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(+numeros [i]);
+        }
+        System.out.println();
+
+        long numero1[]= {23423,54534,656464};
+        for (int i = 0; i < numero1.length; i++) {
+            System.out.print(+ numero1 [i]);
+        }
+        System.out.println();
+        char letra[] = {'a', 'b', 'c'};
+        for (int i = 0; i < letra.length; i++) {
+            System.out.print(+letra [i]);
+        }
+        System.out.println();
+        String palabra[]= new String[3];
+        palabra[0]="pedro";
+        palabra[1]="hipotenusa";
+        palabra[2]="Divide";
+        for (int i = 0; i < palabra.length; i++) {
+            System.out.print(palabra [i] + " ");
+        }
+        System.out.println();
+
+}
+
+
 }
